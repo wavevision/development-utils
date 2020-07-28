b9d70d8161f9fa3e048b7b6c554bcb386d5b8707
Kryštof Řezáč <krystofrezac@gmail.com>
2020-07-28 12:35:54 +0200

    Format code

--- src/DevelopmentUtils/ForcedProductionMode.php
+++ src/DevelopmentUtils/ForcedProductionMode.php
@@ -4,14 +4,23 @@ namespace Wavevision\DevelopmentUtils;
 
 use Nette\Neon\Neon;
 use Nette\StaticClass;
+use Nette\Utils\FileSystem;
+use Wavevision\Utils\Arrays;
 
-class ForcedProductionMode{
-    use StaticClass;
+class ForcedProductionMode
+{
 
-    public static function detect(string $configPath):bool {
-        $localConfig = Neon::decode(file_get_contents($configPath));
-        if($localConfig['parameters']['forceProductionMode'])
-            return true;
-        return false;
-    }
+	use StaticClass;
+
+	/**
+	 * @param string[] $parameterPath
+	 */
+	public static function detect(
+		string $configPath,
+		array $parameterPath = ['parameters', 'forceProductionMode']
+	): bool {
+		$config = Neon::decode(FileSystem::read($configPath));
+		return Arrays::getNestedValue($config, ...$parameterPath) === true;
+	}
+
 }
