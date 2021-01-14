27d450c289e04582bb2fc509b27e7e9ad682444d
Jakub Filla <jakub.filla@wavevision.cz>
2021-01-14 15:44:45 +0100

    Add populate-database

--- src/DevelopmentUtils/Database.php
+++ src/DevelopmentUtils/Database.php
@@ -12,10 +12,17 @@ class Database
 
 	use StaticClass;
 
+	public static function populate(string $configFile, string $pathToDbDump): void
+	{
+		$databaseConfig = self::databaseConfig($configFile);
+		$databaseName = $databaseConfig['name'];
+		$mysql = self::mysql($databaseConfig);
+		$mysql('mysql', "$databaseName < $pathToDbDump");
+	}
+
 	public static function create(string $configFile): void
 	{
-		$config = NeonConfig::read($configFile);
-		$databaseConfig = $config['parameters']['database'];
+		$databaseConfig = self::databaseConfig($configFile);
 		$databaseName = $databaseConfig['name'];
 		$mysql = self::mysql($databaseConfig);
 		$mysql('mysql', "-e 'DROP DATABASE IF EXISTS `$databaseName`'");
@@ -33,6 +40,14 @@ class Database
 		return function (string $base, string $command) use ($config): void {
 			self::bash(implode(' ', [$base, self::mysqlConfig($config), $command]));
 		};
+	}
+
+	/**
+	 * @return array<mixed>
+	 */
+	private static function databaseConfig(string $configFile): array
+	{
+		return NeonConfig::read($configFile)['parameters']['database'];
 	}
 
 	private static function bash(string $command): void
